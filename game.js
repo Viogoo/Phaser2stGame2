c2b275b9a9daed02eab8787d32db0a865380a40c
Viogoo <158023320+Viogoo@users.noreply.github.com>
2024-03-12 13:53:01 +0200

    gjikt

--- game.js
+++ game.js
@@ -19,19 +19,14 @@ var config = {
 
 
 var game = new Phaser.Game(config);
+//var cursors;
+var keyboard;
 var score = 0;
 var scoreText;
 var worldWidth = 9600;
-function hitBomb(player, bomb) {
-    this.physics.pause();
 
-    player.setTint(0xff0000)
-    player.anims.play('turn');
 
-    gameOver = true;
-}
 
-  
 
 
 
@@ -48,107 +43,66 @@ function preload() {
     this.load.image('tree', 'assets/3.png');
     this.load.image('ts', 'assets/5.png');
     this.load.image('star', 'assets/star.png');
-    this.load.spritesheet('dude','assets/2.png',{ frameWidth: 32, frameHeight: 48 } );
+    this.load.spritesheet('dude', 'assets/2.png', { frameWidth: 32, frameHeight: 48 });
 }
 //Додаємо спрайти до сцени
-    function create() {
-   this.add.tileSprite(0, 0, worldWidth, 1080, "sky")
-    .setOrigin (0, 0)
-    .setScale (1)
-    .setDepth (0);
-
-  
- 
-//додаємо генепвцію платформ
-  platforms = this.physics.add.staticGroup();
-
-  for (var x = 0; x < worldWidth; x = x + 128){
-    platforms
-    .create (x, 1050, "ground")
-    .setOrigin (0, 0)
-    .refreshBody (1);
-  }
-       //додаємо кущі
-    
-     
- for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(1700, 100)) {
-    ts= this.physics.add.staticGroup()
-    ts 
-    .create(x, 1080 - 25, 'ts')
-    .setOrigin (0, 1)
-    .setScale (Phaser.Math.FloatBetween(1, 1,5))
-    .setDepth (Phaser.Math.Between(9, 10));
-
-    console.log(ts.X, ts.Y)
+function create() {
+    this.add.tileSprite(0, 0, worldWidth, 1080, "sky")
+        .setOrigin(0, 0)
+        .setScale(1)
+        .setDepth(0);
 
 
-     }
-    
+
+    //додаємо генепвцію платформ
+    platforms = this.physics.add.staticGroup();
+
+    for (var x = 0; x < worldWidth; x = x + 128) {
+        platforms
+            .create(x, 1050, "ground")
+            .setOrigin(0, 0)
+            .refreshBody(1);
+    }
+    //додаємо кущі
+
+
+    for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(1700, 100)) {
+        ts = this.physics.add.staticGroup()
+        ts
+            .create(x, 1080 - 25, 'ts')
+            .setOrigin(0, 1)
+            .setScale(Phaser.Math.FloatBetween(1, 1, 5))
+            .setDepth(Phaser.Math.Between(9, 10));
+
+        console.log(ts.X, ts.Y)
+
+
+    }
+
     //додаємо дерево
-     tree = this.physics.add.staticGroup();
-     
-     for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(2000, 500)) {
+    tree = this.physics.add.staticGroup();
+
+    for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(2000, 500)) {
 
 
-    tree
-        .create(x, 1080 - 25, 'tree')
-        .setOrigin (0, 1)
-        .setScale (Phaser.Math.FloatBetween(1.5, 1.7))
-        .setDepth (Phaser.Math.Between(1, 2));
+        tree
+            .create(x, 1080 - 25, 'tree')
+            .setOrigin(0, 1)
+            .setScale(Phaser.Math.FloatBetween(1.5, 1.7))
+            .setDepth(Phaser.Math.Between(1, 2));
 
         console.log(tree.X, tree.Y)
 
-     }
+    }
 
 
-     //додаємо очки рахунку
-      cursors = this.add.text(16, 16, "score: 0 / 80", {fontSize: "32px", fill: "#000"})
-      stars = this.physics.add.group({
-        key: "star",
-        reperat: 10,
-        setXY: {x: Phaser.Math.FloatBetween(700, 1000), y: 0, stepX: Phaser.Math.FloatBetween(900, 1500) },
-        });
 
-        stars.children.iterate(function (child) {
-            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-        });
-        this.physics.add.collider(stars, platforms);
-        this.physics.add.overlap(player, stars, collectStar, null, this);
-        function collectStar(player, star) {
-             star.disableBody(true, true);
-        }
-
-   //Додаємо рахунок
-   scoreText = this.add.text(16, 16, "score 0 / 80", {fontSize: "32px", fill: "#000"}
-   )
-   .setScrollFactor(0);
-   function collectStar(player, star) {
-    star.disableBody(true, true);
-
-    score += 10;
-    scoreText.setText("Score:" + score + " 0 / 80" ); 
-   }
-   //життя
- livesText = this.add.text(1700, 16, "lives = 3", {fontSize: "32px", fill: "#000"}
- )
- .setScrollFactor(0);
- function collectSkelet(player, skelet) {
-    skelet.disableBody(true, true)
-
-    lives += 1;
-    livesTextText.setText("lives: "- lives)
- }
-   
- 
- 
- 
- 
- //Гравець
+    //Гравець
     player = this.physics.add.sprite(100, 700, 'dude');
 
     player
         .setBounce(0.2)
-        .setDepth (Phaser.Math.Between(4, 5))
+        .setDepth(Phaser.Math.Between(4, 5))
         .setCollideWorldBounds(true);
 
     player.body.setGravityY(300)
@@ -183,16 +137,45 @@ function preload() {
 
     this.cameras.main.setBounds(0, 0, worldWidth, 1080);
     this.physics.world.setBounds(0, 0, worldWidth, 1080);
-    
+
     this.cameras.main.startFollow(player);
+
+    //додаємо очки рахунку
+    cursors = this.input.keyboard.createCursorKeys();
     
+    //
+    stars = this.physics.add.group({
+        key: "star",
+        reperat: 10,
+        setXY: { x: Phaser.Math.FloatBetween(700, 1000), y: 0, stepX: Phaser.Math.FloatBetween(900, 1500) },
+    });
+
+    stars.children.iterate(function (child) {
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.overlap(player, stars, collectStar, null, this);
+    function collectStar(player, star) {
+        star.disableBody(true, true);
+    }
+
+    //Додаємо рахунок
+    scoreText = this.add.text(16, 16, "score 0 / 80", { fontSize: "32px", fill: "#100" }
+    )
+        .setScrollFactor(0);
+    
+    //життя
+    livesText = this.add.text(1700, 16, "lives = 3", { fontSize: "32px", fill: "#000" }
+    )
+        .setScrollFactor(0);
+
 
 };
 
-    function update() {
+function update() {
     //Додаємо керування гравцем
     if (cursors.left.isDown) {
-       player.setVelocityX(-1000);
+        player.setVelocityX(-1000);
 
         player.anims.play('left', true);
     }
@@ -209,5 +192,31 @@ function preload() {
 
     if (cursors.up.isDown && player.body.touching.down) {
         player.setVelocityY(-520);
-   }
     }
+}
+
+function collectStar(player, star) {
+    star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText("Score:" + score + " 0 / 80");
+}
+
+
+
+function collectSkelet(player, skelet) {
+    skelet.disableBody(true, true)
+
+    lives += 1;
+    livesTextText.setText("lives: " - lives)
+}
+
+
+function hitBomb(player, bomb) {
+    this.physics.pause();
+
+    player.setTint(0xff0000)
+    player.anims.play('turn');
+
+    gameOver = true;
+}
